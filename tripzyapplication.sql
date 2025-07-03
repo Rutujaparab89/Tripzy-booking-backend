11470292c12dcf9f851b460e28cdf96a7ceb195f
Rutujaparab89 <rutujaparab26@gmail.com>
2025-07-03 22:15:25 +0530

    add SQL schema file defining database entities and tables

---
+++ tripzyapplication.sql
@@ -0,0 +1,14 @@
+create database tripzy_db;
+use tripzy_db;
+show tables;
+select * from customers;
+select * from destination;
+select * from agency1;
+select * from booking;
+select * from accomodation;
+select * from booking_accomodation;
+select * from activity;
+select * from venue;
+
+
+
